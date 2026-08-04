5a1736f6a03bd333aefc11b64f2dc39ab4a47a78
shawntoadvine <shawntoadvine@gmail.com>
2026-08-04 15:41:34 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='glp1-tracker-v17-meal-parser-save-fix';
+const CACHE='glp1-tracker-v17-persistence-repair';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./avatar.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
